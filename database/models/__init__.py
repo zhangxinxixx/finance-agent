dc0674e03e22bbeb35ebf4ea2deaeb87b1efb2c5
zhangxinxixx <65487887+zhangxinxixx@users.noreply.github.com>
2026-06-17 20:57:32 +0800

    Export execution observability models

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -9,6 +9,7 @@ from .analysis import (
     ensure_analysis_tables,
 )
 from .cme import CmeOptionRow, CmeParseRun, CmeRawFile
+from .execution import ExecutionBase, ExecutionEvent, RunArtifact, ensure_execution_tables
 from .playbook import PlaybookTemplate
 from .report import ReportArtifact, ReportBase, ReportItem, ensure_report_tables
 from .task import Base, StepStatus, TaskRun, TaskStatus, TaskStep
@@ -24,6 +25,11 @@ __all__ = [
     "CmeOptionRow",
     "CmeParseRun",
     "CmeRawFile",
+    # execution observability models
+    "ExecutionBase",
+    "ExecutionEvent",
+    "RunArtifact",
+    "ensure_execution_tables",
     # analysis models
     "AnalysisBase",
     "AnalysisSnapshot",
